50055cf9832e744a86a358fec73db5442c3b65c0
zuriel0001 <dogbeykwame.kd@gmail.com>
2023-06-06 12:33:20 +0000

    fixed output error

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -4,4 +4,4 @@
 
 for alphabets in range(97, 123):
     if chr(alphabets) != 'q' and chr(alphabets) != 'e':
-        print("{} ".format(chr(alphabets)), end="")
+        print("{}".format(chr(alphabets)), end="")
