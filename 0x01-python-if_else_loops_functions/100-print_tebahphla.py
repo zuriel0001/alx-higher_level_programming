54e1188eba86099b7b09ef46e06699b726db8bd0
zuriel0001 <dogbeykwame.kd@gmail.com>
2023-06-06 13:53:22 -0700

    lower and upper case char in reverse

---
+++ 0x01-python-if_else_loops_functions/100-print_tebahphla.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+""""Print alphabet in reverse order alternating upper- and lower-case."""
+
+for ascii_number in range(122, 96, -1):
+	if ascii_number % 2 == 1:
+		ascii_number -= 32
+
+	print("{:c}".format(ascii_number), end="")
