aa1b04d769f96709cc7df332b47fb441ac0ee45e
zuriel0001 <dogbeykwame.kd@gmail.com>
2023-06-06 14:11:45 -0700

    fixed pycodestyle error

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -3,7 +3,7 @@
 """"Print alphabet in reverse order alternating upper- and lower-case."""
 
 for ascii_number in range(122, 96, -1):
-	if ascii_number % 2 == 1:
-		ascii_number -= 32
+    if ascii_number % 2 == 1:
+        ascii_number -= 32
 
-	print("{:c}".format(ascii_number), end="")
+    print("{:c}".format(ascii_number), end="")
