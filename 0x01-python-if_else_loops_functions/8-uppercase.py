412e2ca4c48932c555ebdb34f24a6d5b9ce0402c
zuriel0001 <dogbeykwame.kd@gmail.com>
2023-06-06 12:54:08 -0700

    fixed output error

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -7,4 +7,4 @@ def uppercase(str):
     for alphabet in str:
         if ord(alphabet) >= 97 and ord(alphabet) <= 122:
             alphabet = chr(ord(alphabet) - 32)
-        print("{}".format(alphabet), end="\n")
+        print("{}".format(alphabet), end="")
