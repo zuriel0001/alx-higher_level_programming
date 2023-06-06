3d46dac172314e20702b186e902a546e5f5133dd
zuriel0001 <dogbeykwame.kd@gmail.com>
2023-06-06 14:38:15 +0000

    Fizz Buzz

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+# 12-fizzbuzz.py
+
+
+
+def fizzbuzz():
+    for muner in range(1, 101):
+        if number % 15 == 0:
+            print("FizzBuzz ", end="")
+        elif number % 3 == 0:
+            print("Fizz ", end="")
+        elif number % 5 == 0:
+            print("Buzz ", end="")
+        else:
+            print("{}" .format(number), end="")
