a811fbb55f0df2642bfa4aac3f2c2eb8052e06d5
zuriel0001 <dogbeykwame.kd@gmail.com>
2023-06-08 18:58:42 +0000

    fixed output error

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -4,8 +4,7 @@ if __name__ == "__main__":
     """print all defined hidden_4 module names"""
     import hidden_4
 
-    all_names = dir(hidden_4)
-
-    for name in all_names:
+    names = dir(hidden_4)
+    for name in names:
         if name[:2] != "__":
             print(name)
