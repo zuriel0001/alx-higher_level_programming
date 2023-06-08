ac7b7a217546d4544149d67bc874cdbfbae83678
zuriel0001 <dogbeykwame.kd@gmail.com>
2023-06-08 15:42:40 +0000

    basic arithmetics

---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+
+    """sum, difference, product and quotient of two integers"""
+    from calculator_1 import add, sub, mul, div
+
+    a = 10
+    b = 5
+
+    print("{} + {} = {}".format(a, b, add(a, b)))
+    print("{} - {} = {}".format(a, b, sub(a, b)))
+    print("{} * {} = {}".format(a, b, mul(a, b)))
+    print("{} / {} = {}".format(a, b, div(a, b)))
