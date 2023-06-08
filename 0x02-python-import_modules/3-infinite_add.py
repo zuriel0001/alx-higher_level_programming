f0eae0a368fffebae54c39c387056f7d96541caf
zuriel0001 <dogbeykwame.kd@gmail.com>
2023-06-08 17:10:32 +0000

    sum numbers passed to program on cmd line

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+
+    """print the sum of all arguments passed to a program"""
+    import sys
+
+    sum_total = 0
+
+for i in range(len(sys.argv) - 1):
+        sum_total += int(sys.argv[i + 1])
+
+print("{}".format(sum_total))
