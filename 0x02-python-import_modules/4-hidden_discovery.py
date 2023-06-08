985ba36f7a990fa37d2d2886f30926e128823cb6
zuriel0001 <dogbeykwame.kd@gmail.com>
2023-06-08 18:10:07 +0000

    prints all the names defined by the compiled module hidden_4.pyc

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -4,8 +4,8 @@ if __name__ == "__main__":
     """print all defined hidden_4 module names"""
     import hidden_4
 
-    module_names = dir(hidden_4)
+    names = dir(hidden_4)
 
-    for name in module_names:
+    for name in names:
         if name[:2] is not "__":
             print(name)
