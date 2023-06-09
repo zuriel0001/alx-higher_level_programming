b9b8ed6af05dbf70b63f4ea76e36a5e44a756437
zuriel0001 <dogbeykwame.kd@gmail.com>
2023-06-09 23:38:49 +0000

    retrieve element at index

---
+++ 0x03-python-data_structures/1-element_at.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+"""retrieve number at an index"""
+def element_at(my_list, idx):
+    if idx < 0:
+        return(None)
+    if idx > (len(my_list)-1):
+        return(None)
+    return(my_list[idx])
+
