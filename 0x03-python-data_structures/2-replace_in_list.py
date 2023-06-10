b04c756964e027ab17ae171bb1aecd4f3b5b41ad
zuriel0001 <dogbeykwame.kd@gmail.com>
2023-06-10 17:59:01 +0000

    place item at index

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -2,11 +2,12 @@
 
 """insert element at index"""
 
+
 def replace_in_list(my_list, idx, element):
     if idx < 0:
-        return(my_list)
-    if idx > (len(my_list) -1):
-        return(my_list)
+        return (my_list)
+    if idx > (len(my_list) - 1):
+        return (my_list)
 
     my_list[idx] = element
-    return(my_list)
+    return (my_list)
