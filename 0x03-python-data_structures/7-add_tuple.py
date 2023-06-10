9cf0ca113e970687380ce6a3789dda83f7467fa4
zuriel0001 <dogbeykwame.kd@gmail.com>
2023-06-10 17:25:46 +0000

    add tuples

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python3
 
 
+"""function that adds 2 tuples"""
 def add_tuple(tuple_a=(), tuple_b=()):
     x_comp = ""
     y_comp = ""
 
-    tuple_a += (0, 0)
-    tuple_b += (0, 0)
+    tuple_a += (0, 0)
+    tuple_b += (0, 0)
 
-    x_comp = tuple_a[0] + tuple_b[0]
-    y_comp = tuple_a[1] + tuple_b[1]
+    x_comp = tuple_a[0] + tuple_b[0]
+    y_comp = tuple_a[1] + tuple_b[1]
 
-    return(x_comp, y_comp)
+    return(x_comp, y_comp)
