e4031bee6f7137f07fca681d53f63ffc333ad790
zuriel0001 <dogbeykwame.kd@gmail.com>
2023-06-10 17:05:59 +0000

    add tupple

---
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+
+def add_tuple(tuple_a=(), tuple_b=()):
+    tuple_a += (0, 0)
+    tuple_b += (0, 0)
+
+    x_comp = tuple_a[0] + tuple_b[0]
+    y_comp = tuple_a[1] + tuple_b[1]
+
+    return(x_comp, y_comp)
