fc8bbfc18ca46d852da5ccf736c805705989ed29
zuriel0001 <dogbeykwame.kd@gmail.com>
2023-06-11 02:37:19 +0000

    test file

---
+++ 0x03-python-data_structures/test/9-main.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+max_integer = __import__('9-max_integer').max_integer
+
+my_list = [1, 5, 2, 13, 34, 90, -13, 33]
+max_value = max_integer(my_list)
+print("Max: {}".format(max_value))
