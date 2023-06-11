51cec46e669daeb9acf9fe653efde22292fafae7
zuriel0001 <dogbeykwame.kd@gmail.com>
2023-06-11 00:58:08 +0000

    return length and first letter of sentence

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -2,8 +2,7 @@
 
 
 def multiple_returns(sentence):
-    for i in sentence:
-        if len(sentence) == 0:
-            return (0, None)
-        else:
-            return (len(sentence), sentence[0])
+    if len(sentence) == 0:
+        return (0, None)
+    else:
+        return (len(sentence), sentence[0])
