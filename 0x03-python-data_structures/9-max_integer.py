f08d01b56b56d84399f3d9a3b69886c9fd812826
zuriel0001 <dogbeykwame.kd@gmail.com>
2023-06-11 02:34:13 +0000

    return largest number in list

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -4,10 +4,10 @@
 def max_integer(my_list=[]):
     """ return largest number in a list """
 
-    largest = my_list[0]
-    if len(my_list) == "":
+    if len(my_list) == 0:
         return (None)
 
+    largest = my_list[0]
     for i in range(len(my_list)):
         if my_list[i] > largest:
             largest = my_list[i]
