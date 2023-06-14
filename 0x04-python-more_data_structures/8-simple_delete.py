924526f4249ee44ce0f89b011375878a3eec4ca5
zuriel0001 <dogbeykwame.kd@gmail.com>
2023-06-14 21:17:20 +0000

    function that deletes a key in a dictionary.

---
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def simple_delete(a_dictionary, key=""):
+
+    """function that deletes a key in a dictionary."""
+    new_dictionary = a_dictionary.copy()
+    if key in new_dictionary:
+        del new_dictionary[key]
+
+    return (new_dictionary)
