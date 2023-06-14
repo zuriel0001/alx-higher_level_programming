7888d0871d0ad88b6a20d95eaf6431886e19a183
zuriel0001 <dogbeykwame.kd@gmail.com>
2023-06-14 21:23:03 +0000

    fixed output error

--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -4,7 +4,7 @@ def simple_delete(a_dictionary, key=""):
 
     """function that deletes a key in a dictionary."""
     new_dictionary = a_dictionary.copy()
-    if key in new_dictionary:
+    if key and key in new_dictionary:
         del new_dictionary[key]
 
     return (new_dictionary)
