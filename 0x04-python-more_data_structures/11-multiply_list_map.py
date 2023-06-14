bcbecb9d05601d5cf783999bdd361768c9339343
zuriel0001 <dogbeykwame.kd@gmail.com>
2023-06-14 22:14:39 +0000

    multipy list of integers by a number

---
+++ 0x04-python-more_data_structures/11-multiply_list_map.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+
+
+def multiply_list_map(my_list=[], number=0):
+    """a function that returns a list with all values multiplied by a number"""
+    return (list(map((lambda i: i * number), my_list)))
