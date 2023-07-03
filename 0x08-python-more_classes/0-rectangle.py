ef0e5d153dbf08165928f8d1a4c3a421beee40d6
zuriel0001 <125594301+zuriel0001@users.noreply.github.com>
2023-07-03 20:30:47 +0000

    Update 0-rectangle.py

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
-# 0-rectangle.py
-# Zuriel0001
 """Defines a Rectangle class."""
 
 
 class Rectangle:
-    """Represent a rectangle."""
+    """empty class Rectangle that defines a rectangle"""
     pass
