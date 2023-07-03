f9d02ac9c401abce90e4c1e5ce6cbcfd965cfef7
zuriel0001 <dogbeykwame.kd@gmail.com>
2023-07-03 11:31:28 -0700

    How many instances-fixed output error

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -5,6 +5,7 @@
 class Rectangle:
     """Represent a rectangle."""
 
+    """num_of_instances (int): The number of Rectangle instances"""
     num_of_instances = 0
 
     def __init__(self, width=0, height=0):
@@ -16,6 +17,7 @@ class Rectangle:
         """
         self.width = width
         self.height = height
+        type(self).num_of_instances += 1
 
     @property
     def width(self):
