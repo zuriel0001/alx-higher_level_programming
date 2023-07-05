6134da8505e875eb8573ee3042ac3781dde068a7
zuriel0001 <125594301+zuriel0001@users.noreply.github.com>
2023-07-05 09:08:47 +0000

    Fixing docstring error

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -3,9 +3,10 @@
 
 def add_integer(a, b=98):
     """a function that adds 2 integers.
-    a and b must be first casted to integers if they are float.
+       a and b must be first casted to integers if they are float.
     Raises:
-        TypeError: If either of a or b is a non-integer and non-float """
+        TypeError: If either of a or b is a non-integer and non-float
+    """
 
     if type(a) not in (int, float):
         raise TypeError("a must be an integer")
