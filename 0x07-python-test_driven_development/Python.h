e1fc47f8a85602b5a685550492498edbd9a849a1
zuriel0001 <dogbeykwame.kd@gmail.com>
2023-07-06 14:20:08 -0700

    output eroor

---
+++ 0x07-python-test_driven_development/Python.h
@@ -0,0 +1,11 @@
+#ifndef PYTHON_H
+#define PYTHON_H
+
+#include <stdio.h>
+#include <string.h>
+#include <Python.h>
+#include <stdlib.h>
+
+void print_python_string(PyObject *p);
+
+#endif /* PYTHON_H */
