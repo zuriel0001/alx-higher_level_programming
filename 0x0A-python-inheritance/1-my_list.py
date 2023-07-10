3d5beaf2c943d66bfbf92e2bbfd03a0bb3b3690d
zuriel0001 <125594301+zuriel0001@users.noreply.github.com>
2023-07-10 17:08:56 +0000

    Update 1-my_list.py

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -5,12 +5,6 @@
 class MyList(list):
     """Implements sorted printing for the built-in list class."""
 
-    def __init__(self, *args):
-        if len(args) == 1 and isinstance(args[0], list):
-            super().__init__(args[0])
-        else:
-            super().__init__(args)
-
     def print_sorted(self):
-        sorted_list = sorted(self, key=lambda x: (isinstance(x, int), x))
-        print(sorted_list)
+        """Prints a sorted list"""
+        print(sorted(self))
