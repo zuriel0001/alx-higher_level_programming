2b9a7be81bec257a253ea2bf215d4e99820c9d08
zuriel0001 <125594301+zuriel0001@users.noreply.github.com>
2023-07-11 20:11:19 +0000

    Update 10-student.py

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -11,7 +11,7 @@ class Student:
         self.last_name = last_name
         self.age = age
 
-    def to_json(self):
+    def to_json(self, attrs=None):
         if atrrs is None:
             return (self.__dict__)
         new__dic = {}
