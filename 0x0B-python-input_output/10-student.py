f0b81f53f1743e5f6aef0c11b9cf83d2d09f949a
zuriel0001 <dogbeykwame.kd@gmail.com>
2023-07-11 20:03:25 +0000

    Student to JSON with filter

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Define the clasd Student"""
+
+
+class Student:
+    """Represent the class Student"""
+
+    def __init__(self, first_name, last_name, age):
+        """Initialising objects of the class"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        if atrrs is None:
+            return (self.__dict__)
+        new__dic = {}
+        [new__dic[key] = value for key, value in self.__dict__.items() if key in attrs]
+        return (new__dic)
