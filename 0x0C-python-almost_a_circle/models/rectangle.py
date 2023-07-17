8701bb6eeb47ee8942eee525d1c56001a29eff24
zuriel0001 <dogbeykwame.kd@gmail.com>
2023-07-17 16:31:24 +0000

    pycodestyle compliance

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -128,7 +128,7 @@ class Rectangle(Base):
           None
 
         """
-        if args is not None and len(args) is not 0:
+        if args is not None and len(args) != 0:
             list_atr = ['id', 'width', 'height', 'x', 'y']
             for i in range(len(args)):
                 setattr(self, list_atr[i], args[i])
