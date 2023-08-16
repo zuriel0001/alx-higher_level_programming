bb85a08f2ad5d49d75709386f1452f38f9710c87
zuriel0001 <125594301+zuriel0001@users.noreply.github.com>
2023-08-16 21:16:57 +0000

    Update 0-privileges.sql

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,3 +1,2 @@
 -- Script that lists all privileges of the users user_0d_1 and user_0d_2.
-SHOW GRANTS FOR 'user_0d_1'@'localhost';
-SHOW GRANTS FOR 'user_0d_2'@'localhost';
+SELECT * FROM mysql.user WHERE User IN ('user_0d_1', 'user_0d_2');
