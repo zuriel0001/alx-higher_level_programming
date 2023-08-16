fa8c89052cea3dcee2c57c6b1dd213d429394592
zuriel0001 <125594301+zuriel0001@users.noreply.github.com>
2023-08-16 05:02:13 +0000

    Update 10-top_score.sql

--- 0x0D-SQL_introduction/10-top_score.sql
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -1,5 +1,3 @@
 -- Lists all records of the table second_table.
 -- Results should display both the score and the name in descending score.
-SELECT `score`, `name`
-FROM `second_table`
-ORDER BY `score` DESC;
+SELECT `score`, `name` FROM `second_table` ORDER BY `score` DESC;
