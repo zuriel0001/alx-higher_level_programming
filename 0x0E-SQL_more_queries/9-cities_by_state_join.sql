77b195ddd8874cd2273855dd19d224474e1448b1
zuriel0001 <125594301+zuriel0001@users.noreply.github.com>
2023-08-16 18:21:30 +0000

    Cities by States updated

---
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -0,0 +1,4 @@
+-- Scripts that lists all cities contained in the database hbtn_0d_usa
+-- Each record should display: cities.id - cities.name - states.name
+-- Results is sorted in ascending order by cities.id
+SELECT cities.id, cities.name, states.name FROM cities LEFT JOIN states ON states.id = cities.state_id ORDER BY cities.id;
