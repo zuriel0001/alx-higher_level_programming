99bc5762b8ab00b2a52de15d6e028787e313d217
zuriel0001 <125594301+zuriel0001@users.noreply.github.com>
2023-08-16 19:21:54 +0000

    Update 1-create_user.sql

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,5 +1,4 @@
 -- creates the MySQL server user user_0d_1 and grant all priviledges and password user_0d_1_pwd
 CREATE USER IF NOT EXISTS user_0d_1@localhost IDENTIFIED BY 'user_0d_1_pwd';
 -- user_0d_1 should have all privileges on your MySQL server
-GRANT ALL PRIVILEGES ON * . * TO user_0d_1@localhost IDENTIFIED BY 'user_0d_1_pwd';
-FLUSH PRIVILEGES;
+GRANT ALL PRIVILEGES ON * . * TO user_0d_1@localhost;
