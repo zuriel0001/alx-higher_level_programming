bbefe22ca2e8669161575b704ed84bc4ab9e0388
zuriel0001 <dogbeykwame.kd@gmail.com>
2023-08-16 21:23:13 +0000

    user permisions

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,3 +1,3 @@
--- Script that lists all privileges of user_0d_1 and user_0d_2.
+-- Lists all privileges of user_0d_1 and user_0d_2.
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
