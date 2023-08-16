ee00e38203f8da207910b174fa77a4dbcf9023ba
zuriel0001 <dogbeykwame.kd@gmail.com>
2023-08-16 21:27:38 +0000

    Root user

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,5 +1,6 @@
 -- creates the MySQL server user user_0d_1 and grant all priviledges and password user_0d_1_pwd
 -- user_0d_1 must have all privileges on your MySQL server
+
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 GRANT ALL PRIVILEGES ON * . * TO 'user_0d_1'@'localhost';
 FLUSH PRIVILEGES;
