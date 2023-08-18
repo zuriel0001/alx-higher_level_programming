8082dcf3c7e154af82e08221ebf45c2946a202aa
zuriel0001 <125594301+zuriel0001@users.noreply.github.com>
2023-08-18 00:37:52 +0000

    Update 1-create_user.sql

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -2,10 +2,10 @@
 SELECT 1 FROM mysql.user WHERE user = 'user_0d_1' LIMIT 1;
 
 -- If the user doesn't exist, create it
-CREATE USER IF NOT EXISTS 'user_0d_1'@'%' IDENTIFIED BY 'user_0d_1_pwd';
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 
 -- Grant all privileges to the user
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'%';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
 
 -- Flush privileges to apply changes
 FLUSH PRIVILEGES;
