05dd26a837048ce19db3b6a47adeea5429a6e517
zuriel0001 <dogbeykwame.kd@gmail.com>
2023-09-12 23:19:18 +0000

    fixed output error

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -1,10 +1,13 @@
 #!/usr/bin/node
 class Rectangle {
   constructor (w, h) {
-    if ((w > 0) && (h > 0)) { [this.width = w, this.height = h] = [w, h]; }
+    if ((w > 0) && (h > 0)) {
+      this.width = w;
+      this.height = h;
+    }
   }
 
-print () {
+  print () {
     for (let i = 0; i < this.height; i++) {
       let s = '';
       for (let j = 0; j < this.width; j++) {
@@ -14,7 +17,7 @@ print () {
     }
   }
 
-rotate () {
+  rotate () {
     const aux = this.width;
     this.width = this.height;
     this.height = aux;
