a91bc38340cdf797769794b4b482ccccc3762c34
zuriel0001 <dogbeykwame.kd@gmail.com>
2023-09-12 10:39:16 +0000

    fixed output error

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,4 @@
 #!/usr/bin/node
-const line = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
-for (let i = 0; i < line.length; i++){
-  console.log(line[i]);
+for (const line of ['C is fun', 'Python is cool', 'JavaScript is amazing']) {
+	console.log(line);
 }
