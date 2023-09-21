fc37b46f861df2e953653963d92bcbc4eeead786
zuriel0001 <dogbeykwame.kd@gmail.com>
2023-09-21 22:03:00 +0000

    List relationship

--- 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 """
-Lists all State objects and corresponding City objects contained in the
-hbtn_0e_101_usa database
+Lists all State objects and corresponding City objects contained in the DB
 """
 import sys
 from relationship_state import Base, State
@@ -19,10 +18,9 @@ if __name__ == '__main__':
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    states =
-    session.query(State).outerjoin(City).order_by(State.id, City.id).all()
+    st = session.query(State).outerjoin(City).order_by(State.id, City.id).all()
 
-    for state in states:
+    for state in st:
         print("{}: {}".format(state.id, state.name))
         for city in state.cities:
             print("    {}: {}".format(city.id, city.name))
