2989b2c3de9fe7ed08475cfacbf2db8fbfa07015
zuriel0001 <dogbeykwame.kd@gmail.com>
2023-09-28 21:47:47 +0000

    fixed error

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -12,23 +12,18 @@ def find_peak(list_of_integers):
 
     if list_of_integers is None or list_of_integers == []:
         return None
-
-    start = 0
-    end = len(list_of_integers)
-    mid = (end - start) // 2 + start
+    lo = 0
+    hi = len(list_of_integers)
+    mid = ((hi - lo) // 2) + lo
     mid = int(mid)
-
-    if end == 1:
+    if hi == 1:
         return list_of_integers[0]
-    if end == 2:
+    if hi == 2:
         return max(list_of_integers)
-
-    if list_of_integers[mid] >= list_of_integers[mid - 1] and \
+    if list_of_integers[mid] >= list_of_integers[mid - 1] and\
             list_of_integers[mid] >= list_of_integers[mid + 1]:
         return list_of_integers[mid]
-
     if mid > 0 and list_of_integers[mid] < list_of_integers[mid + 1]:
         return find_peak(list_of_integers[mid:])
-
     if mid > 0 and list_of_integers[mid] < list_of_integers[mid - 1]:
         return find_peak(list_of_integers[:mid])
