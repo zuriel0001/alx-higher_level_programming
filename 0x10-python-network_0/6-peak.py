e8754647897e6ef57850f0369aa88c2865c671e1
zuriel0001 <dogbeykwame.kd@gmail.com>
2023-09-28 22:46:22 +0000

    fixed error

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 "Model for peak finder"
 
+
 def find_peak(list_of_integers):
     """
     Finds a peak in list_of_integers.
@@ -13,19 +14,24 @@ def find_peak(list_of_integers):
         None if the list is empty or None.
     """
 
-    loi = list_of_integers
-    if loi == []:
+    integers_count = len(list_of_integers)
+
+    if integers_count == 0:
         return None
-    total = len(loi)
-    if total == 1:
-        return loi[0]
-    elif total == 2:
-        return max(loi)
-    middle = int(total / 2)
-    peak = loi[middle]
-    if peak > loi[middle - 1] and peak > loi[middle + 1]:
-        return peak
-    elif peak < loi[middle - 1]:
-        return find_peak(loi[:middle])
+
+    if integers_count == 1:
+        return list_of_integers[0]
+
+    if integers_count == 2:
+        return max(list_of_integers)
+
+    middle_index = int(integers_count / 2)
+    peak_element = list_of_integers[middle_index]
+
+    if peak_element > list_of_integers[middle_index - 1] and
+    peak_element > list_of_integers[middle_index + 1]:
+        return peak_element
+    elif peak_element < list_of_integers[middle_index - 1]:
+        return find_peak(list_of_integers[:middle_index])
     else:
-        return find_peak(loi[middle + 1:])
+        return find_peak(list_of_integers[middle_index + 1:])
