85e41f6d730c3a0e8b18ff4677ad7c68f5e39fac
zuriel0001 <dogbeykwame.kd@gmail.com>
2023-10-24 18:03:49 -0700

    display the status code of a GET request.

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,6 +1,7 @@
 #!/usr/bin/node
 /* script that display the status code of a GET request*/
 const request = require('request');
-request.get(process.argv[2]).on('response', function (response) {
+request.get(process.argv[2]).on('response',
+	function (response) {
   console.log(`code: ${response.statusCode}`);
 });
