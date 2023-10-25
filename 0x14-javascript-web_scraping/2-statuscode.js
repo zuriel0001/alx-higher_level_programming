91d6cb9a28f3f44124ec72d66698a017dce3e775
zuriel0001 <dogbeykwame.kd@gmail.com>
2023-10-24 18:10:48 -0700

    semistandard checked

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,7 +1,6 @@
 #!/usr/bin/node
-/* script to display the status code of a GET request*/
+/* script to display the status code of a GET request */
 const request = require('request');
-request.get(process.argv[2]).on('response',
-	function (response) {
+request.get(process.argv[2]).on('response', function (response) {
   console.log(`code: ${response.statusCode}`);
 });
